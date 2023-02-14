0441d76d6272533725991cd36c9e7b76bbe3669d
Pavan-14 <84263574+Pavan-14@users.noreply.github.com>
2023-02-14 07:34:44 +0100

    Update jtoken.py

--- blog/jtoken.py
+++ blog/jtoken.py
@@ -9,7 +9,7 @@ from blog import schemas
 # HS256 is a symmetric algorithm that shares one secret key between the identity provider and your application.
 # The same key is used to sign a JWT and allow verification that signature.
 
-SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
+SECRET_KEY = "#############################################"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
